d35c505e4e0f9f03c036cce5fb4eb74e886a7b4d
JakubSzukala <burski09@outlook.com>
2022-06-20 03:39:25 +0200

    add initial version of input handler - work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::ReceivedCharacter
+};
+
+
+mod input;
+use input::InputHandler;
+use input::AgentStub;
 
 fn main() {
     App::new()
     .add_system(hello_world)
+    .add_plugins(DefaultPlugins)
+    .add_system(input_handler_wrapper)
     .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+// This is crap, it's only for testing TODO read more on bevy app struct
+fn input_handler_wrapper(keyboard_input: Res<Input<KeyCode>>) {
+    let agent = AgentStub {
+        x: 1,
+        y: 2,
+        lvl: 69,
+        speed: 96
+    };
+    let input_handler = InputHandler {agent: agent};
+
+    let command = input_handler.handle_inputs(keyboard_input);
+    command.execute();
 }
+
+fn hello_world() {
+    println!("Hi");
+}
+
+
