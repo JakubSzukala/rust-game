0b0d1e3a3c99eca96e8011907f9381ca411985da
Gladiu <maurycyk1@tlen.pl>
2022-07-24 17:53:36 +0200

    Added rendering map

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use player::PlayerSetupPlugin;
 mod user_interface;
 use user_interface::UIPlugin;
 
+mod map;
+use map::MapLoadPlugin;
+
 fn main() {
     App::new()
     .insert_resource(WindowDescriptor {
@@ -24,31 +27,6 @@ fn main() {
     .add_plugin(InputHandlerPlugin)
     .add_plugin(UIPlugin)
     .add_plugin(PlayerSetupPlugin)
-    .add_startup_system(setup_physics)
-    .add_system(print_ball_altitude)
+    .add_plugin(MapLoadPlugin)
     .run();
-}
-
-
-
-fn setup_physics(mut commands: Commands) {
-    /* Create the ground. */
-    commands
-        .spawn()
-        .insert(Collider::cuboid(100.0, 0.1, 100.0))
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
-
-    /* Create the bouncing ball. */
-    commands
-        .spawn()
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.7))
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
-}
-
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
-    }
 }
