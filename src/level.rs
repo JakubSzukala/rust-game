016819b12d483b873b6b2f79411110549b1ecb75
Gladiu <maurycyk1@tlen.pl>
2022-08-17 21:27:47 +0200

    Clean up code for collision handling

--- src/level.rs
+++ src/level.rs
@@ -11,7 +11,9 @@ use bevy_rapier3d::{prelude::*, rapier::prelude::ColliderBuilder};
 pub struct Level;  // empty struct is just a marker for easy extraction
 
 // Helper resource for tracking meshes that are used for collisions
-struct CollisionMesh(Handle<Mesh>);
+struct CollisionMesh{
+    handle : Handle<Mesh>
+}
 
 pub struct LevelLoadPlugin;
 impl Plugin for LevelLoadPlugin{
@@ -24,14 +26,15 @@ impl Plugin for LevelLoadPlugin{
 }
 
 fn load_level_models( mut commands: Commands, asset_server: Res<AssetServer>) {
-    let level_collision_mesh: Handle<Mesh> = asset_server.load("map/test/collision.glb#Mesh0");
-    commands.insert_resource(level_collision_mesh);
+
 
 }
 
 fn spawn_level( mut commands: Commands,
                     asset_server: Res<AssetServer>,
                 ) {
+    let level_collision_mesh: Handle<Mesh> = asset_server.load("map/test/collision.glb#Mesh0/Primitive0");
+    //commands.insert_resource(CollisionMesh{handle : level_collision_mesh});
 
     let level_entity_id = commands.spawn().id();
 
@@ -39,11 +42,15 @@ fn spawn_level( mut commands: Commands,
     commands.entity(level_entity_id)
         .insert(Level)
         .insert(RigidBody::Fixed)
+        .insert(level_collision_mesh.clone_weak())
         .insert(Collider::ball(3.0))
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)))
         .with_children(|parent| {
             parent.spawn_scene(asset_server.load("map/test/model.glb#Scene0"));
+
         });
+    commands.insert_resource(level_collision_mesh);
+
 
 }
 
@@ -51,30 +58,31 @@ fn update_level_collision(
     mut ev_asset: EventReader<AssetEvent<Mesh>>,
     mut assets: ResMut<Assets<Mesh>>,
     mut p: Query<&mut Collider, With<Level>>,
+    r: Query<&Handle<Mesh>, With<Level>>,
+
 ) {
     for ev in ev_asset.iter() {
         match ev {
             AssetEvent::Created { handle } => {
-                // an image was modified
-                // a texture was just loaded or changed!
+                    let level_collision_handle = r.single();
+                    let mut level_collider = p.single_mut();
 
-                // WARNING: this mutable access will cause another
-                // AssetEvent (Modified) to be emitted!
-                let mesh = assets.get_mut(handle).unwrap();
-                // ^ unwrap is OK, because we know it is loaded now
 
-                let mut level = p.single_mut();
+                    let mesh = assets.get_mut(handle).unwrap();
+                    // ^ unwrap is OK, because we know it is loaded now
 
-                //*level = Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).unwrap();
-                if !Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).is_none(){
-                    *level = Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).unwrap();
-                }
+                    if !Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).is_none(){
 
-            }
-            AssetEvent::Modified { handle } => {
+                        if *level_collision_handle == *handle {
+                            *level_collider = Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).unwrap();
+                        }
+                    }
 
             }
-            AssetEvent::Removed { handle } => {
+            AssetEvent::Modified { handle : _} => {
+
+            }
+            AssetEvent::Removed { handle : _} => {
                 // an image was unloaded
             }
         }
