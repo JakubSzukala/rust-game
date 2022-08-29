731ffb6be4d5e53b8664a3444456f4757a468e83
JakubSzukala <burski09@outlook.com>
2022-08-29 23:37:38 +0200

    adding enemy, work in progress

--- src/enemies/mod.rs
+++ src/enemies/mod.rs
@@ -6,25 +6,34 @@ mod creature;
 pub mod worm;
 pub mod rock;
 
-//use creature::{Health, BasicAD, MovementSpeed};
-
-//use self::worm::{worm_attack, worm_move};
-use self::rock::{rock_attack, rock_move};
+use self::rock::{RockEnemy, rock_follow_player};
 
 pub struct EnemiesPlugin;
 
 impl Plugin for EnemiesPlugin {
     fn build(&self, app: &mut App) {
         app
-            //.add_system(worm_attack)
-            //.add_system(worm_move)
-            .add_system(rock_attack)
-            .add_system(rock_move);
+            .add_system(rock_follow_player)
+            .add_startup_system(RockEnemy::<spawn>);
+        // TODO !!!!!!!!!!!!!!!!!!!!
+        // Use this as example on how to implement EnemiesPlugin to use traits
+        // for attack, move etc
+        // https://github.com/bevyengine/bevy/blob/main/crates/bevy_render/src/camera/projection.rs
+        // https://bevy-cheatbook.github.io/cookbook/custom-projection.html
+        // https://bevy-cheatbook.github.io/patterns/generic-systems.html
     }
 }
 
-trait EnemyTraits { // TODO: add a model handle or something
-    fn spawn(commands: &mut Commands, transform: Transform, asset_server: Res<AssetServer>) -> Entity;
+trait EnemyTraits { 
+    fn spawn(
+        commands: &mut Commands,
+        transform: Transform,
+        asset_server: Res<AssetServer>) -> Entity;
+}
+
+
+fn spawn_enemies() {
+    RockEnemy::spawn();
 }
 
 
--- src/enemies/rock.rs
+++ src/enemies/rock.rs
@@ -4,23 +4,20 @@ use bevy::prelude::*;
 mod creature;
 use creature::{Health, BasicAD, MovementSpeed};
 
+use crate::player::Player;
+
 use super::EnemyTraits;
 
+#[derive(Component)]
 pub struct RockEnemy;
 
 
-pub fn rock_attack() {
-    info!("Rock attak! Uga buga!");     
-}
-
-
-pub fn rock_move() {
-    info!("Rock tumble!");
-}
-
-
 impl EnemyTraits for RockEnemy {
-    fn spawn(commands: &mut Commands, transform: Transform, asset_server: Res<AssetServer>) -> Entity {
+    fn spawn(
+        commands: &mut Commands,
+        transform: Transform,
+        asset_server: Res<AssetServer>
+        ) -> Entity {
         let rock_enemy_entity_id = commands.spawn().id();
         commands.entity(rock_enemy_entity_id)
             .insert(Health(100.0))
@@ -29,6 +26,38 @@ impl EnemyTraits for RockEnemy {
             .insert_bundle(TransformBundle::from(transform))
             .with_children(|parent| {
                 parent.spawn_scene(asset_server.load("models/player.glb#Scene0"));
-            }).id()
+            })
+            .id()
     }
 }
+
+
+pub fn rock_follow_player(
+    mut rock_q: Query<(&mut Transform, &MovementSpeed), With<RockEnemy>>,
+    player_q: Query<&Transform, With<Player>>,
+    time: Res<Time>
+    ) {
+    let player_pos = player_q.single(); 
+    for (mut rock_transform, ms) in rock_q.iter_mut() {
+        // TODO: Use some A* or smth
+        let move_transform = rock_transform.looking_at(
+            player_pos.translation,
+            Vec3::Y);
+        rock_transform.translation += 
+            move_transform.translation // direction 
+            * ms.0                     // movement spped
+            * time.delta_seconds();    // delta
+        println!("Rock at {:?}", rock_transform.translation);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
