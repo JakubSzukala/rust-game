d0ffc29625903cbbbfb94b9d55ee2e58ca0a7f78
JakubSzukala <burski09@outlook.com>
2022-09-09 22:37:07 +0200

    add event to spawner

--- src/enemies/mod.rs
+++ src/enemies/mod.rs
@@ -1,12 +1,11 @@
 use bevy::prelude::*;
-
+use std::marker::PhantomData;
 #[path = "../creature.rs"]
 mod creature;
-use creature::MovementSpeed;
 use creature::BasicAD;
 use creature::Health;
+use creature::MovementSpeed;
 
-pub mod worm;
 pub mod rock;
 
 use crate::player::Player;
@@ -16,29 +15,29 @@ use crate::player::Player;
 #[derive(Component)]
 pub struct Enemy;
 
-pub struct EnemyActionsPlugin<T: EnemyActions>;
+pub struct EnemyActionsPlugin<T: EnemyActions>(PhantomData<T>);
 // Implement Plugin trait on any EnemyActionsPlugin type that implements EnemyActions trait
 // https://doc.rust-lang.org/book/ch10-02-traits.html#using-trait-bounds-to-conditionally-implement-methods
 impl<T: EnemyActions> Plugin for EnemyActionsPlugin<T> {
     fn build(&self, app: &mut App) {
-        app
-            .add_system(T::follow_player)
-            .add_startup_system(T::spawn);
+        app.add_system(T::follow_player)
+            .add_system(T::go)
+            .add_system(T::spawn)
+            .add_event::<SpawnEnemy>();
     }
 }
 
+pub struct SpawnEnemy(Vec3);
 trait EnemyActions {
-    fn spawn(
-        commands: &mut Commands,
-        transform: Transform,
-        asset_server: Res<AssetServer>) -> Entity;
+    fn spawn(events: EventReader<SpawnEnemy>);
     fn go(
         enemy_q: Query<(&Transform, &MovementSpeed), With<Enemy>>,
-        player_q: Query<&Transform, With<Player>>);
+        player_q: Query<&Transform, With<Player>>,
+    );
     fn attack(
         enemy_q: Query<(&Transform, &BasicAD), With<Enemy>>,
-        player_q: Query<(&Transform, &Health), With<Player>>
-        );
+        player_q: Query<(&Transform, &Health), With<Player>>,
+    );
 }
 
 #[derive(Component)]
@@ -48,20 +47,20 @@ impl EnemyActions for SomeEnem {
     fn spawn(
         commands: &mut Commands,
         transform: Transform,
-        asset_server: Res<AssetServer>) -> Entity {
+        asset_server: Res<AssetServer>,
+    ) -> Entity {
         println!("hi spawn here");
     }
     fn go(
         enemy_q: Query<(&Transform, &MovementSpeed), With<Enemy>>,
-        player_q: Query<&Transform, With<Player>>) {
+        player_q: Query<&Transform, With<Player>>,
+    ) {
         println!("Hi following here");
     }
     fn attack(
         enemy_q: Query<(&Transform, &BasicAD), With<Enemy>>,
-        player_q: Query<(&Transform, &Health), With<Player>>
-        ) {
+        player_q: Query<(&Transform, &Health), With<Player>>,
+    ) {
         println!("Hi atacking here");
     }
 }
-
-
